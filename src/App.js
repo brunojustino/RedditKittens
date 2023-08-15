a4a89f845719cd90eecdf429d8ae053f7a80da00
brunojustino <8812217+brunojustino@users.noreply.github.com>
2023-08-15 12:19:44 -0300

    fixed changes on reddit api

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
 import React, { Component } from "react";
-import "./App.css"
+import "./App.css";
 
-const url = 'https://www.reddit.com/r/Kitten.json'; 
+const url = "https://api.reddit.com/r/kitten.json";
+// let headers = new Headers({
+//   "X-Modhash": "j5xn2ubnym3a5491bc9183764ebe11b8fe7d076dd5efa29b86",
+//   "Access-Control-Allow-Origin": "*",
+//   "Access-Control-Allow-Methods": "GET, POST, PATCH, PUT, DELETE, OPTIONS",
+//   "Access-Control-Allow-Headers": " Origin, Content-Type, X-Auth-Token",
+// });
 
 class App extends Component {
   constructor(props) {
@@ -9,7 +15,7 @@ class App extends Component {
     this.state = {
       error: undefined,
       isLoaded: false,
-      items: []
+      items: [],
     };
 
     this.getData = this.getData.bind(this);
@@ -19,34 +25,37 @@ class App extends Component {
   }
 
   getData() {
-    return fetch(url, {mode: 'cors'})
+    return fetch(url, {
+      mode: "cors",
+    })
       .then((res) => res.json())
       .then(
         (result) => {
-        result.data.children.forEach((d) => {
-            const regex = /(https?:\/\/.*\.(?:png|jpg))/i
-            if(regex.test(d.data.url)){
-                this.setState({
-                    items: [...this.state.items, d.data.url],
-                    isLoaded: true
-                })
+          console.log(result);
+          result.data.children.forEach((d) => {
+            const regex = /(https?:\/\/.*\.(?:png|jpg))/i;
+            if (regex.test(d.data.url)) {
+              this.setState({
+                items: [...this.state.items, d.data.url],
+                isLoaded: true,
+              });
             }
-        })
+          });
         },
         (error) => {
           this.setState({
             isLoaded: true,
-            error
-          })
+            error,
+          });
         }
       );
   }
 
   render() {
     const imgStyle = {
-        width: '500px',
-        padding: '5px'
-    }
+      width: "500px",
+      padding: "5px",
+    };
     const { error, isLoaded, items } = this.state;
     //console.log(items)
     if (error) {
@@ -56,19 +65,19 @@ class App extends Component {
     } else {
       return (
         <>
-        <div className="header">
-          <h1> Reddit Kittens</h1>
-          <p>Top /r/Kitten from Reddit</p>
-        </div>
-        <div className="gallery">
-          <ul>
-            {items.map((items) => (
-              <li>
-                <img alt="cute kitten" src={items} />
-              </li>
-            ))}
-          </ul>
-        </div>
+          <div className="header">
+            <h1> Reddit Kittens</h1>
+            <p>Top /r/Kitten from Reddit</p>
+          </div>
+          <div className="gallery">
+            <ul>
+              {items.map((items) => (
+                <li>
+                  <img alt="cute kitten" src={items} />
+                </li>
+              ))}
+            </ul>
+          </div>
         </>
       );
     }
